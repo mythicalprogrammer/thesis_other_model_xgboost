7363d6a5f8b48820c7d5a5c110a9375a0e281321
Anthony Doan <mythicalprogrammer@gmail.com>
2019-02-05 01:48:56 -0800

    Learned how to train xgboost. Most of the process was to convert dataframe into matrix. Cannot have label to be character have to be numerical for sparse matrix.

---
+++ 10_playing.R
@@ -0,0 +1,34 @@
+prostate_data <-
+  read.csv(
+    "start_data/GDS1390_after_anova.csv",
+    header = TRUE,
+    stringsAsFactors = FALSE
+  )
+
+test <- prostate_data[, !names(prostate_data) %in% c("state")]
+label <- prostate_data$state
+
+for (i in 1:length(label)) {
+  if (label[i] == "D") {
+   label[i] <- 0
+  } else {
+   label[i] <- 1
+  }
+}
+
+test <- data.matrix(test)
+label <- data.matrix(label)
+
+rand_seed <- 1030
+set.seed(rand_seed)
+
+
+# train a model using our training data
+model <- xgboost(
+  data = test,
+  label = label,
+  # the data
+  nround = 2,
+  # max number of boosting iterations
+  objective = "binary:logistic"
+)
